3f33245655cedd281d190fd9c2109a65e266f97b
liyuex <1193979065@qq.com>
2016-09-22 17:55:13 +0800

    s

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-
-import Routers from './Routers.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'highlight.js/styles/atelier-seaside-dark.css';
-import './main.css';
-
-render(<Routers />,document.getElementById('root'));
